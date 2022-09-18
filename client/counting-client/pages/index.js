cb3745bf1460646ca2a5f998bd2748b4c8b79826
phillip-kemper <phillip-kemper@gmx.net>
2022-09-18 18:00:23 +0200

    remove unnecessary items pt1.

--- client/counting-client/pages/index.js
+++ client/counting-client/pages/index.js
@@ -1,53 +1,24 @@
-import Link from "next/link";
 import Head from "next/head";
 import React from "react";
+import { Typography } from "@mui/material";
 
 export default function Home() {
   return (
-    <div className="container">
+    <div
+      className="container"
+      style={{
+        backgroundColor: "grey",
+      }}
+    >
       <Head>
         <title>Mount Count</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Read <Link href="/posts/first-post">this page</Link>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <Typography variant={"h2"}>Current count</Typography>
+        <Typography variant="h3">25</Typography>
+        <Typography variant="h4">Submit new count</Typography>
       </main>
 
       <style jsx>{`

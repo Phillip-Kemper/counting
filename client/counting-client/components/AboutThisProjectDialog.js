27e23e7ef5dd2821cf1249316880f850953c7b07
phillip-kemper <phillip-kemper@gmx.net>
2022-09-22 20:35:32 +0200

    style box a little bit

--- client/counting-client/components/AboutThisProjectDialog.js
+++ client/counting-client/components/AboutThisProjectDialog.js
@@ -1,13 +1,33 @@
-import { Dialog, DialogTitle } from "@mui/material";
+import {
+  Box,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+} from "@mui/material";
 import React from "react";
+
+const PaperComponent = (props) => {
+  return <Box style={{ backgroundColor: "grey" }}>{props.children}</Box>;
+};
 
 export const AboutThisProjectDialog = (props) => {
   const { closeDialog, open } = props;
 
   return (
-    <Dialog onClose={closeDialog} open={open}>
+    <Dialog
+      maxWidth={"sm"}
+      onClose={closeDialog}
+      open={open}
+      PaperComponent={PaperComponent}
+    >
       <DialogTitle>About This Project</DialogTitle>
-      <p> hi</p>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-slide-description">
+          Let Google help apps determine location. This means sending anonymous
+          location data to Google, even when no apps are running.
+        </DialogContentText>
+      </DialogContent>
     </Dialog>
   );
 };
